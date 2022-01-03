05f494d19cd994208965a20b814cfe98ae1a0e17
Meri <mgogi13@freeuni.edu.ge>
2022-01-03 19:08:34 +0400

    Intro to state

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    return <div>Hi there!</div>;
-};
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+
+        //We initialize state
+        this.state = { lat: null, errorMessage: '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                // we use setState to update state
+                this.setState({ lat: position.coords.latitude })
+            },
+            (error) => {
+                this.setState({ errorMessage: error.message });
+            }
+        );
+    }
+    render(){
+        if(this.state.errorMessage && !this.state.lat) {
+            return <div>Error: { this.state.errorMessage }</div>
+        }
+
+        if(!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: { this.state.lat }</div>
+        }
+
+        return <div>loading...</div>
+    }
+}
 
 ReactDOM.render(
     <App />,
